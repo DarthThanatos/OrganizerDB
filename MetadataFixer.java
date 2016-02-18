818fe947801a0e0d76b1b012adf1046ca17f0333
Robert <bielas.robert95@gmail.com>
2016-02-18 01:36:54 +0100

    fixer indentation

--- MetadataFixer.java
+++ MetadataFixer.java
@@ -12,36 +12,35 @@ public class MetadataFixer {
 	}
 	
 	public static boolean recursiveVistit(String dirName){
-        System.out.println("IN DIR " + dirName);
+		System.out.println("IN DIR " + dirName);
 		File folder = new File(dirName);
 		File[] listOfFiles = folder.listFiles();
 		String metadataContent = "";
-	    for (int i = 0; i < listOfFiles.length; i++) {
-	      if (listOfFiles[i].isFile()) {
-	        System.out.println("File " + listOfFiles[i].getName());
-	        if(!listOfFiles[i].getName().endsWith(".txt")){
-		        System.out.println("Deleting file " + listOfFiles[i].getName());
-		        listOfFiles[i].delete();
-	        }
-	        else{
-	        	if(!listOfFiles[i].getName().equals( "metadata.txt")){
-	        		System.out.println(listOfFiles[i].getName() + " != " + "metadata.txt");
-	        		metadataContent += listOfFiles[i].getName() + "\n";
-	        	}
-	        }
-	      } else if (listOfFiles[i].isDirectory()) {
-	    	  if(recursiveVistit(dirName + "//"+ listOfFiles[i].getName())) {
-	    		  if(new File(dirName + "//" + listOfFiles[i].getName()).delete()) System.out.println("Deleted");
-	    		  else System.out.println("NOT    Deleted");
-	    	  }
-	      }
-	    }
-
+		for (int i = 0; i < listOfFiles.length; i++) {
+		if (listOfFiles[i].isFile()) {
+			System.out.println("File " + listOfFiles[i].getName());
+			if(!listOfFiles[i].getName().endsWith(".txt")){
+				System.out.println("Deleting file " + listOfFiles[i].getName());
+				listOfFiles[i].delete();
+			}
+			else{
+				if(!listOfFiles[i].getName().equals( "metadata.txt")){
+					System.out.println(listOfFiles[i].getName() + " != " + "metadata.txt");
+					metadataContent += listOfFiles[i].getName() + "\n";
+				}
+			}
+		} else if (listOfFiles[i].isDirectory()) {
+			if(recursiveVistit(dirName + "//"+ listOfFiles[i].getName())) {
+				if(new File(dirName + "//" + listOfFiles[i].getName()).delete()) System.out.println("Deleted");
+				else System.out.println("NOT    Deleted");
+				}
+			}
+		}
 		try{
-				BufferedWriter metaWriter = new BufferedWriter(new FileWriter(dirName + "//metadata.txt"));
-				metaWriter.write(metadataContent);
-				metaWriter.close();
-				if (metadataContent.equals("")) new File(dirName + "//metadata.txt").delete();
+			BufferedWriter metaWriter = new BufferedWriter(new FileWriter(dirName + "//metadata.txt"));
+			metaWriter.write(metadataContent);
+			metaWriter.close();
+			if (metadataContent.equals("")) new File(dirName + "//metadata.txt").delete();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
